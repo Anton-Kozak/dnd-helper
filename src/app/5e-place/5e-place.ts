0dfd2c7b1ee9a2a3f1dd37f8a68c0dcab7580ce9
anton.kozak <akozak@lenovo.com>
2025-11-12 17:46:17 +0100

    pr: upgrade angualr to 20, create 5e placeholder component

--- src/app/5e-place/5e-place.ts
+++ src/app/5e-place/5e-place.ts
@@ -1,11 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import * as MonsterInfo from '../../resources/bestiary/bestiary-mm.json';
 
 @Component({
   selector: 'app-5e-place',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './5e-place.html',
   styleUrls: ['./5e-place.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FivePlace {}
+export class FivePlaceComponent implements AfterViewInit {
+  monsterData = '';
+  monsterFormGroup: FormGroup = new FormGroup({
+    monsterName: new FormControl(''),
+  });
+
+  ngAfterViewInit(): void {
+    this.monsterFormGroup
+      .get('monsterName')
+      ?.valueChanges.pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((searchValue: string) => {
+        console.log('value', searchValue);
+        this.searchForMonsterInfo(searchValue);
+      });
+  }
+
+  searchForMonsterInfo(subStr: string): void {
+    MonsterInfo.monster.map((monster) => {
+      if (monster.name.toLowerCase().includes(subStr)) {
+        this.monsterData =
+          (monster.reprintedAs && monster.reprintedAs[0]) || 'N/A';
+        return;
+      }
+    });
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FivePlaceComponent } from './5e-place/5e-place';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FivePlaceComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
