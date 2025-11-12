1fe0bd2e61271d57cc4f9570629b3d74e923bd2a
anton.kozak <akozak@lenovo.com>
2025-11-12 16:29:17 +0100

    init

---
+++ src/app/5e-place/5e-place.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-5e-place',
+  standalone: true,
+  imports: [],
+  templateUrl: './5e-place.html',
+  styleUrls: ['./5e-place.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FivePlace {}
